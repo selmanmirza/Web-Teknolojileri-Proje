309003601f6bd3ccb05db51b0e82266f93a6e25f
selmanmirza <selmandemirtas618@gmail.com>
2025-05-14 14:20:01 +0300

    İletişim kismi
    İletişim kısmı yapıldı.

---
+++ Proje 5.kisim/iletisim.php
@@ -0,0 +1,18 @@
+<?php
+
+    $isim = $_POST["isim"];
+    $soyisim = $_POST["soyisim"];
+    $eposta = $_POST["eposta"];
+    $telno = $_POST["tel"];
+    $cinsiyet = $_POST["cinsiyet"];
+    $mesaj = $_POST["mesaj"];
+
+    echo "İsim: $isim <br>";
+    echo "Soyisim: $soyisim <br>";
+    echo "E-posta: $eposta <br>";
+    echo "Telefon: $telno <br>";
+    echo "Cinsiyet: $cinsiyet <br>";
+    echo "Mesaj: $mesaj <br>";
+
+
+?>
