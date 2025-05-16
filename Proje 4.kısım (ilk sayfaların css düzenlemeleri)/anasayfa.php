b2d75ddfd1c994ced97dedd50ad92bbbfb126211
selmanmirza <selmandemirtas618@gmail.com>
2025-05-16 16:32:15 +0300

    Anasayfa
    Phpde değişiklik yaptim

--- Proje 4.kısım (ilk sayfaların css düzenlemeleri)/anasayfa.php
+++ Proje 4.kısım (ilk sayfaların css düzenlemeleri)/anasayfa.php
@@ -9,6 +9,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       header('Location: anasayfa.html');
       exit();
     }
+    if(isset($_POST['iletisim'])){
+      header ('Location: iletisim.html');
+      exit();
+    }
+    if(isset($_POST['sehrim'])){
+      header ('Location: sehrim.html');
+      exit();
+    }
+    if(isset($_POST['cv'])){
+      header ('Location: cv.html');
+      exit();
+    }
+    if(isset($_POST['miras'])){
+      header ('Location: miras.html');
+      exit();
+    }
+    if(isset($_POST['ilgi'])){
+      header('Location: ilgi.html');
+      exit();
+    }
 }
 
 
